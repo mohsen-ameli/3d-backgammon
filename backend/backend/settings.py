84f7d1e4185e68e92354e7196ae8684c8326840a
mohsen-ameli <mohsen.ameli83@gmail.com>
2023-02-19 15:01:04 -0500

    procfile added and static files re-ran

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -182,7 +182,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
