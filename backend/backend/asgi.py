630cde50793a2bfb801c1f7f9604db4c7fe2fc57
mohsen-ameli <mohsen.ameli83@gmail.com>
2023-02-20 01:24:36 -0500

    Configured asgi.py

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,23 +1,17 @@
-"""
-ASGI config for backend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
-"""
-
-import os
+import os, django
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+
+# This will make sure settings is ready, and django is running
+django.setup()
+
 import game.routing
 import users.routing
-import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-django.setup()
+
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
