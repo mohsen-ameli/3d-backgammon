05862de9d591f78aa3937799622d10439d41e88a
mohsen-ameli <mohsen.ameli83@gmail.com>
2023-02-20 00:39:56 -0500

    Debugging websocket connection

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -77,7 +77,8 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)] if DEBUG else [os.environ.get("REDIS_URL")],
+            # "hosts": [("127.0.0.1", 6379)] if DEBUG else [os.environ.get("REDIS_URL")],
+            "hosts": ["redis://default:bmrdERwh0EuC80j1srGK@containers-us-west-23.railway.app:7610"]
         },
     },
 }
--- frontend/src/components/utils/getServerUrl.js
+++ frontend/src/components/utils/getServerUrl.js
@@ -1,9 +1,7 @@
 const getServerUrl = (http = true) => {
   if (process.env.NODE_ENV === "development") {
-    console.log("Development ", http)
     return http ? "http://localhost:8000" : "ws://localhost:8000"
   } else {
-    console.log("Production ", http)
     return http ? "https://3d-backgammon.up.railway.app" : "wss://3d-backgammon.up.railway.app" // prettier-ignore
   }
 }
