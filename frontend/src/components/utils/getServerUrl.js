31edc62e128b31aab1a978abe15a47e8eab15cc1
mohsen-ameli <mohsen.ameli83@gmail.com>
2023-02-19 21:38:16 -0500

    switched to wss instead of ws

--- frontend/src/components/utils/getServerUrl.js
+++ frontend/src/components/utils/getServerUrl.js
@@ -2,7 +2,7 @@ const getServerUrl = (http = true) => {
   if (process.env.NODE_ENV === "development") {
     return http ? "http://localhost:8000" : "ws://localhost:8000"
   } else {
-    return http ? "https://3d-backgammon.up.railway.app" : "ws://3d-backgammon.up.railway.app" // prettier-ignore
+    return http ? "https://3d-backgammon.up.railway.app" : "wss://3d-backgammon.up.railway.app" // prettier-ignore
   }
 }
 
