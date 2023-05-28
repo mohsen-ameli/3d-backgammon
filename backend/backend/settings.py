85db9d6610cc9c949d6a51ce5552c80c781bc20b
mohsen-ameli <mohsen.ameli83@gmail.com>
2023-05-28 03:05:33 -0400

    Changed the id field to bigautofield in the backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -93,12 +93,12 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+   DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.sqlite3',
+           'NAME': BASE_DIR / 'db.sqlite3',
+       }
+   }
 else:
     DATABASES = {
         "default": {
